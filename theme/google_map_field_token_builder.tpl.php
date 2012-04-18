df1fcdb7920bb6149583113773664d0100c1f45b
scot.hubbard <scot.hubbard@868214.no-reply.drupal.org>
2012-04-18 10:20:25 +0100

    Added token feature and token builder.

---
+++ theme/google_map_field_token_builder.tpl.php
@@ -0,0 +1,56 @@
+<?php
+
+  // add the google API
+  drupal_add_js('http://maps.googleapis.com/maps/api/js?sensor=false', 'external');
+
+  $lat = 51.5171;
+  $lon = -0.15;
+  $zoom = 9;
+
+  $js = '
+    var map;
+    function setMarker(latLng) {
+    	marker.setPosition(latLng);
+      map.setCenter(latLng);
+    }
+    function buildToken() {
+      mrkrPos = marker.getPosition();
+      var latlng = new google.maps.LatLng(mrkrPos);
+      latitude = latlng.lat;
+      longitude = latlng.lng;
+      zoom = map.getZoom();
+      width = document.getElementById("width").value;
+      height = document.getElementById("height").value;
+      document.getElementById("token").value = "[gmf:lat="+latitude+",lon="+longitude+",width="+width+",height="+height+",zoom="+zoom+"]";
+    }
+    jQuery(document).ready(function(){
+      var latlng = new google.maps.LatLng(' . $lat . ', ' . $lon . ');
+      var mapOptions = {
+        zoom: ' . $zoom . ',
+        center: latlng,
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById("google_map_picker"), mapOptions);
+      marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+      });
+      // add a click listener to the map
+      google.maps.event.addListener(map, "click", function(event) {
+        setMarker(event.latLng)
+        buildToken();
+      });
+      google.maps.event.addListener(map, "zoom_changed", function(event) {
+      	buildToken();
+      });
+  	});
+  ';
+  drupal_add_js($js, 'inline');
+
+	print '<div id="google_map_picker" style="width: 500px; height: 400px;"></div>';
+	print '<input type="text" id="width" name="width" value="300"/>';
+	print '<input type="text" id="height" name="height" value="250"/><br/>';
+	print '<input style="width: 100%;" type="text" id="token" name="token" readonly="readonly" value="[gmf:lat=51.5171,lon=-0.15,width=300,height=250,zoom=9]"/>';
+
+?>

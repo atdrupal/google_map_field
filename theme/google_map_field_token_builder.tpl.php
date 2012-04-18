393e7bb8860fb73b668d8d1cc64896c94f19a7f3
scot.hubbard <scot.hubbard@868214.no-reply.drupal.org>
2012-04-18 11:49:24 +0100

    Fixed issue with token buider and issue with multiple inline maps not displaying.

--- theme/google_map_field_token_builder.tpl.php
+++ theme/google_map_field_token_builder.tpl.php
@@ -14,14 +14,11 @@
       map.setCenter(latLng);
     }
     function buildToken() {
-      mrkrPos = marker.getPosition();
-      var latlng = new google.maps.LatLng(mrkrPos);
-      latitude = latlng.lat;
-      longitude = latlng.lng;
+      var mrkrPos = marker.getPosition();
       zoom = map.getZoom();
       width = document.getElementById("width").value;
       height = document.getElementById("height").value;
-      document.getElementById("token").value = "[gmf:lat="+latitude+",lon="+longitude+",width="+width+",height="+height+",zoom="+zoom+"]";
+      document.getElementById("token").value = "[gmf:lat="+mrkrPos.lat()+",lon="+mrkrPos.lng()+",width="+width+",height="+height+",zoom="+zoom+"]";
     }
     jQuery(document).ready(function(){
       var latlng = new google.maps.LatLng(' . $lat . ', ' . $lon . ');
@@ -48,9 +45,12 @@
   ';
   drupal_add_js($js, 'inline');
 
+  print '<p>Find the center point for the map you want to insert into you content and click to drop a marker there...</p>';
 	print '<div id="google_map_picker" style="width: 500px; height: 400px;"></div>';
-	print '<input type="text" id="width" name="width" value="300"/>';
-	print '<input type="text" id="height" name="height" value="250"/><br/>';
+  print '<p>Now set the width and height for the inline map...</p>';
+	print '<input type="text" id="width" name="width" value="300" onchange="return buildToken();"/>';
+	print '<input type="text" id="height" name="height" value="250" onchange="return buildToken();"/><br/>';
+	print '<p>Here is your token, copy and paste this into the content where you want this map to appear...</p>';
 	print '<input style="width: 100%;" type="text" id="token" name="token" readonly="readonly" value="[gmf:lat=51.5171,lon=-0.15,width=300,height=250,zoom=9]"/>';
 
 ?>

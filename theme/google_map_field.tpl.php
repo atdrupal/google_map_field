d8bddfdb7cb0fe7926e569a7d97c80d5b49ce93c
scot.hubbard <scot.hubbard@868214.no-reply.drupal.org>
2012-04-24 09:21:20 +0100

    Removed incorrectly named tpl file.

--- theme/google_map_field.tpl.php
+++
@@ -1,7 +0,0 @@
-
-<div class="google-map-field">
-  <div class="google-map-field-label">
-    [[<?php print $name; ?>]]
-  </div>
-  <div id="google_map_field_<?php print $delta; ?>" style="width: <?php print $width; ?>px; height: <?php print $height; ?>px;"></div>
-</div>

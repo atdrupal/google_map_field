a7f4f1c4b030a25e6f36a02a129f9062133faed7
scot.hubbard <scot.hubbard@868214.no-reply.drupal.org>
2012-04-25 20:41:15 +0100

    Fixed validation issues on google map field latitude and longitude fields.

--- js/google_map_field_edit_form.js
+++ js/google_map_field_edit_form.js
@@ -1,15 +1,15 @@
 
 function buildToken() {
   var $ = jQuery.noConflict();
-  $("#edit-zoom").val(map.getZoom());
-  $("#edit-token").val("lat=" + marker.getPosition().lat() + ",lon=" + marker.getPosition().lng() + ",width=" + $("#edit-width").val() + ",height=" + $("#edit-height").val() + ",zoom=" + map.getZoom());
+  $("#edit-zoom").val(mapTokenBuilder.getZoom());
+  $("#edit-token").val("lat=" + marker.getPosition().lat() + ",lon=" + marker.getPosition().lng() + ",width=" + $("#edit-width").val() + ",height=" + $("#edit-height").val() + ",zoom=" + mapTokenBuilder.getZoom());
   return false;
 }
 function setMarker(latLng) {
   marker.setPosition(latLng);
-  map.setCenter(latLng);
+  mapTokenBuilder.setCenter(latLng);
 }
-var map;
+var mapTokenBuilder;
 function getMap() {
   var latlng = new google.maps.LatLng(51.0, 0.12);
   var mapOptions = {
@@ -18,16 +18,16 @@ function getMap() {
     streetViewControl: false,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  map = new google.maps.Map(document.getElementById("google_map_field_selector"), mapOptions);
+  mapTokenBuilder = new google.maps.Map(document.getElementById("google_map_field_selector"), mapOptions);
   marker = new google.maps.Marker({
     position: latlng,
-    map: map,
+    map: mapTokenBuilder,
   });
-  google.maps.event.addListener(map, "click", function(event) {
+  google.maps.event.addListener(mapTokenBuilder, "click", function(event) {
     setMarker(event.latLng)
     buildToken();
   });
-  google.maps.event.addListener(map, "zoom_changed", function(event) {
+  google.maps.event.addListener(mapTokenBuilder, "zoom_changed", function(event) {
     buildToken();
   });
 };
